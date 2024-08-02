1f1f771e65cd9d8c01f48dc29c5cece1c7822733
Reza <r.malek@keemail.me>
2024-08-02 10:49:08 +0200

    add nonsuccess response body to error | cleanup prometheus pushgateway

--- utility.go
+++ utility.go
@@ -4,42 +4,17 @@ import (
 	"errors"
 	"fmt"
 	"github.com/go-resty/resty/v2"
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/push"
 	"time"
 )
-
-// Create a new Pusher targeting the Pushgateway
-var pusher = push.New(fmt.Sprintf("http://%s:%s", host, "9091"), "umbilical-choir")
 
 func checkResponse(fn func() (*resty.Response, error, time.Duration)) (string, error, time.Duration) {
 	resp, err, runTime := fn()
 	if err != nil {
 		return "", err, runTime
 	}
-	if !resp.IsSuccess() {
-		msg := fmt.Sprintf("non-successful response (%d)", resp.StatusCode())
+	if !resp.IsSuccess() { // if not in 2xx range
+		msg := fmt.Sprintf("non-successful response (%d): %v", resp.StatusCode(), resp.String())
 		return "", errors.New(msg), runTime
 	}
 	return string(resp.Body()), nil, runTime
-}
-
-//func PushResponseTime(metric prometheus.Gauge, duration time.Duration) error {
-//	// Update the metric locally
-//	metric.Set(float64(duration) / float64(time.Millisecond))
-//
-//	// Set job and groupings (optional)
-//	pusher.Grouping("program", program)
-//
-//	return pusher.Collector(metric).Add()
-//}
-
-func PushMetrics(metrics ...prometheus.Gauge) error {
-	// Set job and groupings (optional)
-	pusher.Grouping("program", program)
-
-	for _, metric := range metrics {
-		pusher.Collector(metric)
-	}
-	return pusher.Add()
 }
